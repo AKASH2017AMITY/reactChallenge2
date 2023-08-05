edc6945555072067baf67becc7f1c3102fc8c2eb
akash kumar <120650503+AKASH2017AMITY@users.noreply.github.com>
2023-08-05 22:44:41 +0530

    final submit

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import styles from "./Contactform.module.css"
+// import Button from '../Button/Button'
+
+
 
 function ContactForm() {
   return (
-    <section className={styles.container}>
+    <>
+      <section className={styles.container}>
         <div className={styles.contactForm}>
-            
+
+        
         </div>
         <div className={styles.contactImage}>
-            <img src="" alt="" />
+          <img id={styles.mainImage} src="/images/mainImage.jpg" alt="MainImage" />
         </div>
-    </section>
+      </section>
+
+    </>
   )
 }
 
